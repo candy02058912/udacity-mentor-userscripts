04733cd9f744ef5be3ad732e540da680d7e4c780
Candy Tsai <candy02058912@gmail.com>
2019-10-05 13:45:13 +0800

    remove unused code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-const fs = require("fs");
 const path = require("path");
-const template = require("lodash/template");
 const WebpackUserscript = require("webpack-userscript");
 
 module.exports = {
