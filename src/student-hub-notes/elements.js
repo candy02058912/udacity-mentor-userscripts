7cb728fe729b693c0a6743651ba172d5560f8106
mixedconnections <mixed.connections2@gmail.com>
2020-02-22 22:38:27 -0500

    adding dropdown menu

--- src/student-hub-notes/elements.js
+++ src/student-hub-notes/elements.js
@@ -1,4 +1,32 @@
 export const injectELements = () => {
+
+  //Create array of options to be added
+  var optNames =  ['Support request','Dashboard','Quick tips','Message toolkit'];
+  var optValues = ['http://rebrand.ly/mentor-support','https://mentor-dashboard.udacity.com/','https://sites.google.com/udacity.com/udacity-community-guide/resources/student-hub-quick-tips','http://bit.ly/message-template-toolkit'];
+
+  //Create and append select list
+  var selectList = document.createElement("select");
+  selectList.id = "mySelect";
+  var option = document.createElement("option");
+  option.value = "";
+  option.text = 'Mentor useful links';
+  option.disabled = true;
+  option.selected = "selected";
+  selectList.appendChild(option);
+
+  //Create and append the options
+  for (var i = 0; i < optNames.length; i++) {
+    var option = document.createElement("option");
+    option.value = optValues[i];
+    option.text  = optNames[i];
+    selectList.appendChild(option);
+  }
+
+  selectList.onchange = function(){
+    var win = window.open(this.value, '_blank');
+    win.focus();
+  }
+
   const notesDiv = document.createElement("div");
   const notesTitle = document.createElement("h3");
   const notesTextArea = document.createElement("textarea");
@@ -21,5 +49,5 @@ export const injectELements = () => {
 
   notesTitle.append("Notes");
   notesDiv.append(notesTitle, notesTextArea, saveBtn);
-  sidebar.prepend(notesDiv);
+  sidebar.prepend(selectList, notesDiv);
 };
